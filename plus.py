092949fb8c607b028ebdaf1dd4e219d81227dacc
Levelent <keegan.rimmington@gmail.com>
2020-10-15 18:00:58 +0100

    significant re-write

--- plus.py
+++ plus.py
@@ -1,16 +1,17 @@
 from discord.ext import commands, tasks
-from discord import Embed
+from discord import Embed, Member, Object
 from discord.errors import NotFound
 from json import load, dumps
 from asyncio import sleep
 from time import time
+from typing import Optional
+from datetime import datetime
 
 
 # Note that the Message ID has to be stored as a string.
 def get_all_data():
     with open("data.json") as data_f:
         return load(data_f)
-    # TODO - Use BoardMessage objects instead for the value
 
 
 def update_data(data):
@@ -18,40 +19,41 @@ def update_data(data):
         data_f.write(dumps(data))
 
 
-# Only counts messages created in the last X days.
+# Only counts messages created in the last X days. Gets all users.
 def get_recent_data(days):
     data = get_all_data()
     msg_data = data["messages"]
     msg_data_new = {}
     for key, value in msg_data.items():
+        print(key)
+        print(value)
         unix_now = int(time())
-        if (unix_now - key["unix_timestamp"]) < (days * 86400):  # The number of seconds per day
+        if (unix_now - value["unix_timestamp"]) < (days * 86400):  # The number of seconds per day
             msg_data_new[key] = value
 
     return {"messages": msg_data_new, "users": data["users"]}
 
 
 def get_settings():
-    with open("settings.json") as set_f:
+    with open("settings.json", encoding="utf8") as set_f:
         s_data = set_f
-    return load(s_data)
+        return load(s_data)
 
 
 def update_settings(s_data):
-    with open("settings.json", "w") as set_f:
+    with open("settings.json", "w", encoding="utf8") as set_f:
         set_f.write(dumps(s_data))
 
 
 async def true_react_count(reaction):
-    self_react_check = reaction.msg.author in await reaction.users().flatten()
-    # If self-react, subtract 1 from counter
+    self_react_check = reaction.message.author in await reaction.users().flatten()
+    # If self-react, subtract 1 (True) from counter
     return reaction.count - self_react_check
 
 
-class BoardMessage:
-    def __init__(self, unix_timestamp, reaction_count):
-        self.time = unix_timestamp
-        self.reacts = reaction_count
+class ServerSettings:
+    def __init__(self, chl):
+        self.channel = chl
 
 
 class Plus(commands.Cog):
@@ -63,10 +65,15 @@ class Plus(commands.Cog):
         self.chl_id = s_data["chl_id"]
         self.threshold = s_data["threshold"]
         self.emote = s_data["emote"]
+        self.remove_deleted = s_data["remove_deleted"]
 
         # These properties will be uploaded to 'data.json' periodically
-        self.message_store = {}  # '<msg id>': ('timestamp': <unix time int>, 'reacts': <star num>)
-        self.user_store = {}
+
+        # '<msg id>': {"star_msg_id": <int id>, "user_id": <int id>, "unix_timestamp": <int>, "count": <int>}
+        self.message_store = {}
+
+        # '<user id>': {'counted_msgs': [<message ids>], 'total_reacts': <star num>}
+        self.user_store = {}  # TODO: Implement user store
 
         self.update_stores()
         self.data_transfer.start()
@@ -76,9 +83,13 @@ class Plus(commands.Cog):
         self.message_store = data["messages"]
         self.user_store = data["users"]
 
+    def is_remove_deleted(self):
+        return self.remove_deleted
+
     # Grabs previous data from file, updates and writes back to file.
     @tasks.loop(minutes=5)
     async def data_transfer(self):
+        # Transfer message data
         data = get_all_data()
 
         msg_data = data["messages"]
@@ -87,10 +98,20 @@ class Plus(commands.Cog):
 
         user_data = data["users"]
         for key, value in self.user_store.items():
+            # TODO Recalculate numbers from stored messages
             user_data[key] = value
 
         combine = {"messages": msg_data, "users": user_data}
         update_data(combine)
+
+        # Transfer settings data
+        s_data = {
+            "chl_id": self.chl_id,
+            "threshold": self.threshold,
+            "emote": self.emote,
+            "remove_deleted": self.remove_deleted
+        }
+        update_settings(s_data)
 
     @data_transfer.before_loop
     async def before_transfer(self):
@@ -102,59 +123,93 @@ class Plus(commands.Cog):
             return self.bot.get_emoji(self.emote)
         return self.emote
 
+    async def get_star_msg(self, msg_id):
+        star_chl = self.bot.get_channel(self.chl_id)
+        if star_chl is None:
+            print("!! Starboard Channel Deleted.")
+            return None
+        try:
+            star_msg = await star_chl.fetch_message(msg_id)
+        except NotFound:
+            print("! Starboard Message Deleted.")
+            return None
+        return star_msg
+
     # Only cares about messages in the last 14 days
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
-        # TODO: Solve duplicate glitch
+        # Checks for correct emote
         emote = self.get_emote()
-
         if reaction.emoji != emote or self.chl_id is None or user.bot:
             return
+        print(f"Got reaction from {user}")
 
-        print(f"Got: {reaction}")
+        count = await true_react_count(reaction)
 
         if reaction.message.id in self.message_store:
-            msg_tuple = self.message_store[reaction.message.id]
-            star_msg = await self.bot.get_channel(self.chl_id).fetch_message(msg_tuple[0])
-            num = reaction.count
-            if self.bot.get_user(msg_tuple[1]) in await reaction.users().flatten():
-                num -= 1
-            await star_msg.edit(content=f" {str(emote)} **{num}** | {reaction.message.channel.mention}")
+            msg_dict = self.message_store[reaction.message.id]
+            print(msg_dict)
+
+            star_msg = await self.get_star_msg(msg_dict["star_msg_id"])
+            if star_msg is None:
+                return
+
+            await star_msg.edit(content=f"{str(emote)} **{count}** | {reaction.message.channel.mention}")
             return
 
-        if await true_react_count(reaction) >= self.threshold:
-            await self.make_new(reaction.message)
+        # TODO: Solve duplicate glitch?
+
+        if count >= self.threshold:
+            await self.make_new(reaction.message, count)
 
     @commands.Cog.listener()
-    async def on_reaction_remove(self, reaction, user):
+    async def on_reaction_remove(self, reaction, _):
         # TODO: sort out what happens when a user removes a reaction
-        # Note: it might disappear from starboard, or not at all if it's the message author! Might also delete entirely.
-        pass
+        if reaction.message.id not in self.message_store:
+            return
+        count = await true_react_count(reaction)
+        if count < self.threshold:
+            await self.message_store_remove(reaction.message.id)
+        else:
+            self.message_store[reaction.message.id]["count"] = count
 
-    # Also happens if the number of reactions hit 0
-    def message_store_remove(self, msg_id):
-        if message := self.message_store.pop(msg_id, False):
-            print(f"Message deleted: {message}")
+        # Note: it might disappear from starboard, or not at all if it's the message author! Might also delete entirely.
+        # Remove if number of reactions hits drops below threshold.
+
+    async def message_store_remove(self, msg_id):
+        # Won't remove anything from store if the message isn't there
+        msg_dict = self.message_store.pop(msg_id, None)
+        if msg_dict:
+            print(f"Message deleted: {msg_dict}")
+            star_msg = await self.get_star_msg(msg_dict["star_msg_id"])
+            if star_msg is None:
+                return
+
             # Delete the message in the client itself
             # Should also remove in the users where necessary
 
     @commands.Cog.listener()
-    async def on_reaction_clear(self, message, reactions):
-        self.message_store_remove(message.message_id)
-        pass
+    async def on_reaction_clear(self, message, _):  # _ means we ignore the reaction parameter
+        await self.message_store_remove(message.message_id)
 
     @commands.Cog.listener()
+    @commands.check(is_remove_deleted)
     async def on_raw_message_delete(self, payload):
-        self.message_store_remove(payload.message_id)
+        await self.message_store_remove(payload.message_id)
 
     @commands.Cog.listener()
+    @commands.check(is_remove_deleted)
     async def on_raw_bulk_message_delete(self, payload):
         for msg_id in payload.message_ids:
-            self.message_store_remove(msg_id)
+            await self.message_store_remove(msg_id)
 
-    async def make_new(self, react_msg):
-        em = Embed(colour=0x8B008B,
-                   description=f"[[Jump to original]({react_msg.jump_url})]\n\n{react_msg.content}")
+    async def make_new(self, react_msg, count: int):
+        size_limit = min(len(react_msg.content), 2000)
+        em = Embed(
+            colour=0x0a9231,
+            description=f"{react_msg.content[:size_limit]}\n\n[[Jump to original]({react_msg.jump_url})]",
+            timestamp=react_msg.created_at
+        )
         em.set_author(name=str(react_msg.author), icon_url=react_msg.author.avatar_url)
 
         if len(react_msg.attachments) != 0:
@@ -162,24 +217,30 @@ class Plus(commands.Cog):
             # What if it's not an image?
             em.set_image(url=attachments[0].url)
 
-        emote_num = self.threshold
         out_chl = self.bot.get_channel(self.chl_id)
-        out_msg = await out_chl.send(f" {self.get_emote()}**{emote_num}** | {react_msg.channel.mention}", embed=em)
 
-        msg_obj = {"star_msg_id": out_msg.id, "user_id": react_msg.author.id, "unix_timestamp": int(time())}
+        out_msg = await out_chl.send(f" {self.get_emote()} **{count}** | {react_msg.channel.mention}", embed=em)
+
+        msg_obj = {
+            "star_msg_id": out_msg.id,
+            "user_id": react_msg.author.id,
+            "unix_timestamp": int(react_msg.created_at.timestamp()),
+            "count": count
+        }
+        print(f"Created {int(react_msg.created_at.timestamp())} in {react_msg.channel.name} | {count}")
         self.message_store[react_msg.id] = msg_obj
 
-    async def update_count(self, react_msg, log_msg):
-        pass
+        return out_msg
 
     @commands.command()
-    @commands.has_guild_permissions(manage_server=True)
-    async def updateall(self, ctx):
+    @commands.has_guild_permissions(manage_guild=True)
+    async def setup(self, ctx):
         emote = self.get_emote()
 
-        messages = {}
+        messages = []
         for chl in ctx.guild.text_channels:
-            async for msg in chl.history(oldest_first=True):
+            print(chl.name)
+            async for msg in chl.history(limit=None, after=Object(761697871773958175).created_at, oldest_first=True):
                 for react in msg.reactions:
                     if react.emoji == emote:
                         # Check if there's a self-react
@@ -188,16 +249,18 @@ class Plus(commands.Cog):
                         react_num = react.count - self_react_check
 
                         if react_num >= self.threshold:
-                            # Need to put into more permanent structure
-                            messages[msg.created_at] = (msg, react_num)
-                            break
+                            print(react_num)
+                            # TODO: Need to put into more permanent structure
+                            messages.append((int(msg.created_at.timestamp()), msg, react_num))
+                        break
 
-        for msg in sorted(messages):
-            await self.make_new(msg)
+        print(f"{len(messages)} messages total")
+        for _, msg, react_num in sorted(messages, key=lambda item: item[0]):
+            await self.make_new(msg, react_num)
 
-    @commands.command()
+    @commands.command(name="channel")
     @commands.has_guild_permissions(manage_messages=True)
-    async def setchannel(self, ctx, *, target=None):
+    async def set_channel(self, ctx, *, target=None):
         if target is None:
             self.chl_id = None
             await ctx.channel.send("Starboard channel removed.")
@@ -209,9 +272,9 @@ class Plus(commands.Cog):
         self.chl_id = log_chl.id
         await ctx.channel.send(f"Starboard channel updated to: {log_chl.mention}")
 
-    @commands.command()
+    @commands.command(name="threshold")
     @commands.has_guild_permissions(manage_messages=True)
-    async def setthreshold(self, ctx, value="5"):
+    async def set_threshold(self, ctx, value="5"):
         try:
             value = int(value)
         except ValueError:
@@ -220,14 +283,12 @@ class Plus(commands.Cog):
         if value < 1:
             await ctx.send(f"Ah yes, a threshold of {value} messages. That makes perfect sense.")
             return
-        elif value > len(ctx.guild.members):
-            await ctx.send("What you entered there is literally larger than the population of the earth. Get help.")
         self.threshold = value
         await ctx.channel.send(f"Reaction threshold updated to: {value}")
 
-    @commands.command()
+    @commands.command(name="emote")
     @commands.has_guild_permissions(manage_messages=True)
-    async def setemote(self, ctx, emote="⭐"):
+    async def set_emote(self, ctx, emote="⭐"):
         if emote.isdigit():
             e = self.bot.get_emoji(int(emote))
             if e is None:
@@ -245,8 +306,8 @@ class Plus(commands.Cog):
             await ctx.send("What you entered is neither a standard emote, nor a custom emote id.")
 
     @commands.command()
-    async def stats(self, ctx, target_user=None):
-        # TODO If no arguments, return top X users by emote, and top X starred messages (only the IDs).
+    async def stats(self, _, target_user: Optional[Member] = None):
+        # TODO If no arguments, return top X users by 'stars', and top X 'starred' messages (only the IDs).
         if target_user is None:
 
             return
